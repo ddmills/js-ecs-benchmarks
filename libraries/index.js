134922d27f9f35d2ae7897046f50aff9521d24b2
Dalton Mills <mills.dalton@gmail.com>
2021-01-31 12:07:50 -0600

    add tiny-ecs

--- libraries/index.js
+++ libraries/index.js
@@ -4,3 +4,4 @@ export { default as PerformEcsLibrary } from './lib-perform-ecs';
 export { default as ApeEcsLibrary } from './lib-ape-ecs';
 export { default as EcsyLibrary } from './lib-ecsy';
 export { default as NanoEcsLibrary } from './lib-nano-ecs';
+export { default as TinyEcsLibrary } from './lib-tiny-ecs';
---
+++ libraries/lib-tiny-ecs.js
@@ -0,0 +1,63 @@
+import tiny from 'tiny-ecs';
+
+function Position() {
+    this.x = 0;
+    this.y = 0;
+};
+
+function Velocity() {
+    this.dx = 1.2;
+    this.dy = 1.7;
+};
+
+class MovementSystem {
+    updateCount = 0;
+
+    constructor(world) {
+        this.world = world;
+    }
+
+    update() {
+        this.world.queryComponents([Position, Velocity]).forEach((entity) => {
+            entity.position.x += entity.velocity.dx;
+            entity.position.y += entity.velocity.dy;
+            this.updateCount++;
+        });
+    }
+};
+
+export default {
+    name: 'tiny-ecs',
+    setup() {
+        this.world = new tiny.EntityManager();
+
+        this.movementSystem = new MovementSystem(this.world);
+    },
+    createEntity() {
+        return this.world.createEntity();
+    },
+    addPositionComponent(entity) {
+        entity.addComponent(Position);
+    },
+    addVelocityComponent(entity) {
+        entity.addComponent(Velocity);
+    },
+    removePositionComponent(entity) {
+        entity.removeComponent(Position);
+    },
+    removeVelocityComponent(entity) {
+        entity.removeComponent(Velocity);
+    },
+    destroyEntity(entity) {
+        entity.remove();
+    },
+    cleanup() {
+        this.world = null;
+    },
+    updateMovementSystem() {
+        this.movementSystem.update();
+    },
+    geMovementSystemUpdateCount() {
+        return this.movementSystem.updateCount;
+    },
+};
