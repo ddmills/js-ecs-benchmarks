15d05b1de9766faf5f620108c059c8006605fa4d
jprochazk <honza.spacir@gmail.com>
2021-02-18 13:49:46 +0100

    Add uecs

--- libraries/index.js
+++ libraries/index.js
@@ -6,3 +6,4 @@ export { default as ApeEcsLibrary } from './lib-ape-ecs';
 export { default as NanoEcsLibrary } from './lib-nano-ecs';
 export { default as TinyEcsLibrary } from './lib-tiny-ecs';
 export { default as YaglEcsLibrary } from './lib-yagl-ecs';
+export { default as UecsEcsLibrary } from './lib-uecs';
---
+++ libraries/lib-uecs.js
@@ -0,0 +1,62 @@
+import { World } from 'uecs';
+
+class Position {
+    x = 0;
+    y = 0;
+}
+
+class Velocity {
+    dx = 1.2;
+    dy = 1.7;
+}
+
+class MovementSystem {
+    updateCount = 0;
+    view = null;
+
+    constructor(world) {
+        this.view = world.view(Position, Velocity);
+    }
+
+    update() {
+        this.view.each((_, pos, vel) => {
+            pos.x += vel.dx;
+            pos.y += vel.dy;
+            this.updateCount++;
+        });
+    }
+}
+
+export default {
+    name: 'uecs',
+    setup() {
+        this.world = new World();
+        this.movementSystem = new MovementSystem(this.world);
+    },
+    createEntity() {
+        const entity = this.world.create();
+        return entity;
+    },
+    addPositionComponent(entity) {
+        this.world.emplace(entity, new Position());
+    },
+    addVelocityComponent(entity) {
+        this.world.emplace(entity, new Velocity());
+    },
+    removePositionComponent(entity) {
+        this.world.remove(entity, Position);
+    },
+    removeVelocityComponent(entity) {
+        this.world.remove(entity, Velocity);
+    },
+    destroyEntity(entity) {
+        this.world.destroy(entity);
+    },
+    cleanup() {},
+    updateMovementSystem() {
+        this.movementSystem.update();
+    },
+    geMovementSystemUpdateCount() {
+        return this.movementSystem.updateCount;
+    },
+};
