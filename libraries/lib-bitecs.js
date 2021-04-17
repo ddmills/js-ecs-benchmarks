da96fa4fd2b0e1eb943837025a7daf8b86ff0077
Nate <nate.teh.greatt@gmail.com>
2021-04-17 07:44:47 -0700

    bitecs fixes

--- libraries/lib-bitecs.js
+++ libraries/lib-bitecs.js
@@ -1,60 +1,67 @@
-import World from 'bitecs';
+import {
+  createWorld,
+  defineComponent,
+  defineQuery,
+  defineSystem,
+  addComponent,
+  removeComponent,
+  addEntity,
+  removeEntity,
+  Types,
+} from 'bitecs';
 
 let updateCount = 0;
 
 export default {
     name: 'bitecs',
     setup() {
-        this.world = World({ maxEntities: 3000000 });
-        this.world.registerComponent('POSITION', {
-            x: 'float32',
-            y: 'float32',
-        });
-        this.world.registerComponent('VELOCITY', {
-            vx: 'int8',
-            vy: 'int8',
-            speed: 'uint16',
-        });
-        this.world.registerSystem({
-            name: 'MOVEMENT',
-            components: ['POSITION', 'VELOCITY'],
-            update: (POSITION, VELOCITY) => (entities) => {
-                for (let i = 0; i < entities.length; i++) {
-                    const eid = entities[i];
-                    POSITION.x[eid] += VELOCITY.vx[eid] * VELOCITY.speed[eid];
-                    POSITION.y[eid] += VELOCITY.vy[eid] * VELOCITY.speed[eid];
-                    updateCount++;
-                }
-            },
-        });
+        
+        this.world = createWorld()
+
+        const { f32, ui16 } = Types
+        const Vector2 = { x: f32, y: f32 }
+        this.Position = defineComponent(Vector2)
+        this.Velocity = defineComponent({...Vector2, speed: ui16})
+
+        this.query = defineQuery([this.Position,this.Velocity])
+
+        this.move = defineSystem(world => {
+            const ents = this.query(world)
+            for (let i = 0; i < ents.length; i++) {
+                const eid = ents[i]
+                this.Position.x[eid] += this.Velocity.x[eid]
+                this.Position.y[eid] += this.Velocity.y[eid]
+            }
+        })
+          
     },
     createEntity() {
-        return this.world.addEntity();
+        return addEntity(this.world);
     },
     addPositionComponent(entity) {
-        this.world.addComponent('POSITION', entity, { x: 100, y: 100 });
+        addComponent(this.world, this.Position, entity);
+        this.Position.x[entity] = 100
+        this.Position.y[entity] = 100
     },
     addVelocityComponent(entity) {
-        this.world.addComponent('VELOCITY', entity, {
-            vx: 1,
-            vy: -1,
-            speed: 100,
-        });
+        addComponent(this.world, this.Velocity, entity);
+        this.Velocity.x[entity] = 1.2;
+        this.Velocity.x[entity] = 1.7;
     },
     removePositionComponent(entity) {
-        this.world.removeComponent('POSITION', entity);
+        removeComponent(this.world, this.Position, entity);
     },
     removeVelocityComponent(entity) {
-        this.world.removeComponent('VELOCITY', entity);
+        removeComponent(this.world, this.Velocity, entity);
     },
     destroyEntity(entity) {
-        this.world.removeEntity(entity);
+        removeEntity(this.world, entity);
     },
     cleanup() {
         updateCount = 0;
     },
     updateMovementSystem() {
-        this.world.step();
+        this.move(this.world);
     },
     geMovementSystemUpdateCount() {
         return updateCount;
